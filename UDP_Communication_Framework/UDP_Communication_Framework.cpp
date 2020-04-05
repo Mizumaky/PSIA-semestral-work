2b4e756e8de5bb9705b64ebdffab669061170ee8
Müller, Miroslav <mullemi5@fel.cvut.cz>
2020-04-05 17:04:53 +0200

    reciever 1.0

--- UDP_Communication_Framework/UDP_Communication_Framework.cpp
+++ UDP_Communication_Framework/UDP_Communication_Framework.cpp
@@ -5,13 +5,15 @@
 #include "stdafx.h"
 #include <winsock2.h>
 #include "ws2tcpip.h"
+#include <iostream>
+#include <fstream>
 
 #define TARGET_IP	"127.0.0.1"
 
 #define BUFFERS_LEN 1024
 
-#define SENDER
-//#define RECEIVER
+//#define SENDER
+#define RECEIVER
 
 #ifdef SENDER
 #define TARGET_PORT 5555
@@ -30,11 +32,20 @@ void InitWinsock()
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 }
 
+void recievedata(SOCKET s, char* buf, int len, int flags, sockaddr* from, int* fromlen)
+{
+	if (recvfrom(s, buf, len, flags, from, fromlen) == SOCKET_ERROR) {
+		printf("Socket error or timeout :C\n");
+		getchar();
+		exit(1);
+	}
+}
+
 //**********************************************************************
 int main()
 {
 	SOCKET socketS;
-	
+
 	InitWinsock();
 
 	struct sockaddr_in local;
@@ -47,9 +58,9 @@ int main()
 
 
 	socketS = socket(AF_INET, SOCK_DGRAM, 0);
-	if (bind(socketS, (sockaddr*)&local, sizeof(local)) != 0){
+	if (bind(socketS, (sockaddr*)&local, sizeof(local)) != 0) {
 		printf("Binding error!\n");
-	    getchar(); //wait for press Enter
+		getchar(); //wait for press Enter
 		return 1;
 	}
 	//**********************************************************************
@@ -57,16 +68,16 @@ int main()
 	char buffer_tx[BUFFERS_LEN];
 
 #ifdef SENDER
-	
+
 	sockaddr_in addrDest;
 	addrDest.sin_family = AF_INET;
 	addrDest.sin_port = htons(TARGET_PORT);
 	InetPton(AF_INET, _T(TARGET_IP), &addrDest.sin_addr.s_addr);
 
-	
+
 	strncpy(buffer_tx, "Hello world payload!\n", BUFFERS_LEN); //put some data to buffer
-	printf("Sending packet.\n");
-	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));	
+	printf("Sending packet...\n");
+	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
 
 	closesocket(socketS);
 
@@ -74,20 +85,60 @@ int main()
 
 #ifdef RECEIVER
 
-	strncpy(buffer_rx, "No data received.\n", BUFFERS_LEN);
-	printf("Waiting for datagram ...\n");
-	if(recvfrom(socketS, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&from, &fromlen) == SOCKET_ERROR){
-		printf("Socket error!\n");
+	/// Recieve filename
+	memset(buffer_rx, 0, BUFFERS_LEN);
+	printf("Waiting for filename packet...\n");
+	recievedata(socketS, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&from, &fromlen);
+	printf("Data recieved: %s\n", buffer_rx);
+	char filename[BUFFERS_LEN];
+	strncpy(filename, buffer_rx, BUFFERS_LEN);
+
+
+	/// Recieve size of data
+	memset(buffer_rx, 0, BUFFERS_LEN);
+	printf("Waiting for data size packet...\n");
+	recievedata(socketS, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&from, &fromlen);
+	int datalen;
+	if (sscanf(buffer_rx, "%d", &datalen) < 0) {
+		printf("Data to integer conversion error :C\n");
 		getchar();
 		return 1;
 	}
-	else
-		printf("Datagram: %s", buffer_rx);
+	printf("Data recieved: %d\n", datalen);
 
+
+	/// Recieve data
+	std::ofstream file;
+	file.open(filename);
+	if (!file) {
+		printf("Cannot open file for writing :c");
+		getchar();
+		return 1;
+	}
+	int numofpackets = (datalen + 1023) / 1024;
+	for (int i = 1; i < numofpackets + 1; i++) {
+		printf("Waiting for data packet #%d...\n", i);
+		recievedata(socketS, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&from, &fromlen);
+		printf("OK\n");
+		if (i != numofpackets) {
+			file << buffer_rx;
+		}
+		else {
+			int remainingbytes = datalen % 1024;
+			for (int j = 0; j < remainingbytes; j++) {
+				file << buffer_rx[j];
+			}
+		}
+	}
+	file.close();
+
+	/// Close communication
 	closesocket(socketS);
 #endif
-	//**********************************************************************
 
+	/// End program
 	getchar(); //wait for press Enter
-	return 0;
 }
+
+//	char* data = new char[datalen];
+//delete [] data;
