37a0a3206398ea6608b2b3a6c1e655cb344b3fda
mullemi5 <mullemi5@fel.cvut.cz>
2020-05-25 12:14:15 +0200

    functions polish

--- UDP_Communication_Framework/UDP_Communication_Framework.cpp
+++ UDP_Communication_Framework/UDP_Communication_Framework.cpp
@@ -1,6 +1,5 @@
 ﻿#pragma comment(lib, "ws2_32.lib")
 #include "stdafx.h"
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,9 +12,7 @@
 #include "crc32.h"
 #include "sha256.h"
 
-
 // SETTINGS
-
 #define TARGET_IP	"192.168.30.24"
 
 #define START_PACKET_LEN 256
@@ -42,7 +39,6 @@
 #define Y_INFO_OFFSET 7
 
 // INITIAL DECLARATIONS
-
 enum type_t
 {
     START = 0,
@@ -72,43 +68,52 @@ int Y_WT_FAIL = Y_BAR + 3 + Y_FAILS_OFFSET + 1;
 int Y_SP_FAIL = Y_BAR + 3 + Y_FAILS_OFFSET + 2;
 int Y_INFO = Y_BAR + 3 + Y_INFO_OFFSET;
 
-// FUNCTIONS
-
-void setPrintPos(int x, int y)
+// PRINT FUNCTIONS
+void SetPrintPos(int x, int y)
 {
     const COORD position = { short(x), short(y) };
     SetConsoleCursorPosition(h_std_out, position);
 }
-void setPrintPos(COORD position)
+void SetPrintPos(COORD position)
 {
     SetConsoleCursorPosition(h_std_out, position);
 }
-
 void Error(std::string text)
 {
-    setPrintPos(0, Y_INFO);
+    SetPrintPos(0, Y_INFO);
     std::wstring wtext = std::wstring(text.begin(), text.end());
     wprintf(L"ERROR: %s :C                                            \n", wtext.c_str());
     getchar();
 }
 void Warning(std::string text)
 {
-    setPrintPos(0, Y_INFO);
+    SetPrintPos(0, Y_INFO);
     std::wstring wtext = std::wstring(text.begin(), text.end());
     wprintf(L"WARNING: %s                                             \n", wtext.c_str());
 }
 void Info(std::string text)
 {
-    setPrintPos(0, Y_INFO);
+    SetPrintPos(0, Y_INFO);
     std::wstring wtext = std::wstring(text.begin(), text.end());
     wprintf(L"%s                                                      \n", wtext.c_str());
 }
-COORD indexToPos(int index)
+COORD IndexToPos(int index)
 {
     //Info("index = " + std::to_string(index) + "\n screen_width = " + std::to_string(screen_width) + "\n result =" + std::to_string(index % (screen_width - 2) + 1));
     return { short(index % (screen_width - 2) + 1), short(Y_BAR + index / (screen_width - 2)) };
 }
+void InitPrinting()
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    CONSOLE_SCREEN_BUFFER_INFO scrnBufferInfo;
+    GetConsoleScreenBufferInfo(h_std_out, &scrnBufferInfo);
+    screen_width = scrnBufferInfo.srWindow.Right - scrnBufferInfo.srWindow.Left + 1;
+    if (!SetConsoleScreenBufferSize(h_std_out, { short(screen_width), 100 })) {
+        Error("could not prepare screen buffer");
+    }
+}
 
+// CORE FUNCTIONS
 void InitWinsock()
 {
     WSADATA wsaData;
@@ -131,7 +136,6 @@ void InitApp()
         Error("socket binding unsuccessful");
     }
 }
-
 void RecievePacket()
 {
     //memset(com.buffer_rx, 0, DATA_PACKET_LEN);
@@ -157,8 +161,6 @@ void SendACK(int32_t number)
 
     SendPacket();
 }
-
-
 bool isCrcOk(const char* data, size_t len)
 {
     // load received
@@ -174,23 +176,17 @@ bool isCrcOk(const char* data, size_t len)
     return true;
 }
 
-//**********************************************************************
+//***************************** MAIN *****************************************
 int main()
 {
     InitWinsock();
     InitApp();
-    // INIT PRINTING
-    _setmode(_fileno(stdout), _O_U16TEXT);
-    CONSOLE_SCREEN_BUFFER_INFO scrnBufferInfo;
-    GetConsoleScreenBufferInfo(h_std_out, &scrnBufferInfo);
-    screen_width = scrnBufferInfo.srWindow.Right - scrnBufferInfo.srWindow.Left + 1;
-    if (!SetConsoleScreenBufferSize(h_std_out, { short(screen_width), 100 })) {
-        Error("could not prepare screen buffer");
-    }
+    InitPrinting();
+
     // PRINT INTRO
-    setPrintPos(0, 0);
+    SetPrintPos(0, 0);
     wprintf(L"|| Receiver v4.0 ||\n");
-    setPrintPos(0, Y_STATUS);
+    SetPrintPos(0, Y_STATUS);
     wprintf(L"STATUS: waiting for START packet...\n");
 
     // RECIEVE FILES
@@ -198,10 +194,10 @@ int main()
 
         int32_t ack_number = -2;
         int32_t packets_count;
+        bool* is_received;
         unsigned int crc_fail_count = 0;
         unsigned int wrong_type_count = 0;
         unsigned int same_packet_received_count = 0;
-        bool* is_received;
 
         char filename[NAME_LEN + 1] = { '\0' };
         int32_t file_size = 0;
@@ -243,9 +239,9 @@ int main()
         }
 
         // PRINT START
-        setPrintPos(0, Y_STATUS);
+        SetPrintPos(0, Y_STATUS);
         wprintf(L"STATUS: waiting for DATA packets...\n");
-        setPrintPos(0, Y_FILENAME);
+        SetPrintPos(0, Y_FILENAME);
         std::string filename_s = std::string(filename);
         wprintf(L"FILENAME: %s\n", std::wstring(filename_s.begin(), filename_s.end()).c_str());
         wprintf(L"FILE SIZE: %d bytes\n", file_size);
@@ -256,29 +252,29 @@ int main()
         Y_SP_FAIL = Y_BAR + bar_height + Y_FAILS_OFFSET + 2;
         Y_INFO = Y_BAR + bar_height + Y_INFO_OFFSET;
         // --- rows
-        setPrintPos(0, Y_BAR - 1);
+        SetPrintPos(0, Y_BAR - 1);
         wprintf(L"%s", std::wstring(screen_width, '-').c_str());
-        setPrintPos(0, Y_BAR + bar_height);
+        SetPrintPos(0, Y_BAR + bar_height);
         wprintf(L"%s", std::wstring(screen_width, '-').c_str());
         // | | signs
         int remaining_tmp = packets_count;
         int bar_offset = 0;
         while (remaining_tmp > 0) {
-            setPrintPos(0, Y_BAR + bar_offset);
+            SetPrintPos(0, Y_BAR + bar_offset);
             wprintf(L"|");
             if (remaining_tmp >= screen_width - 2) {
-                setPrintPos(screen_width - 1, Y_BAR + bar_offset);
+                SetPrintPos(screen_width - 1, Y_BAR + bar_offset);
                 wprintf(L"|");
             }
             else {
-                setPrintPos(remaining_tmp + 1, Y_BAR + bar_offset);
+                SetPrintPos(remaining_tmp + 1, Y_BAR + bar_offset);
                 wprintf(L"|");
             }
             remaining_tmp -= screen_width - 2;
             bar_offset++;
         }
         // fails
-        setPrintPos(0, Y_CRC_FAIL);
+        SetPrintPos(0, Y_CRC_FAIL);
         wprintf(L"CRC fail count: %d\n", crc_fail_count);
         wprintf(L"Wrong packet type count: %d\n", wrong_type_count);
         wprintf(L"Same packet received count: %d\n", same_packet_received_count);
@@ -290,7 +286,7 @@ int main()
             // TYPE GET/CHECK
             if (type_t(com.packet_rx[CRC_LEN]) != DATA) {
                 SendACK(ack_number);
-                setPrintPos(0, Y_WT_FAIL);
+                SetPrintPos(0, Y_WT_FAIL);
                 wprintf(L"Wrong packet type count: %d\n", ++wrong_type_count);
                 Warning("received packet type not DATA");
                 continue;
@@ -301,10 +297,10 @@ int main()
             // CRC GET/CHECK
             if (!isCrcOk(com.packet_rx, DATA_PACKET_LEN)) {
                 SendACK(ack_number);
-                setPrintPos(0, Y_CRC_FAIL);
+                SetPrintPos(0, Y_CRC_FAIL);
                 wprintf(L"CRC fail count: %d\n", ++crc_fail_count);
                 if (received_num > ack_number && received_num < packets_count) {
-                    setPrintPos(indexToPos(received_num));
+                    SetPrintPos(IndexToPos(received_num));
                     wprintf(L"!");
                 }
                 continue;
@@ -316,7 +312,7 @@ int main()
             }
             if (is_received[received_num]) {
                 SendACK(ack_number);
-                setPrintPos(0, Y_SP_FAIL);
+                SetPrintPos(0, Y_SP_FAIL);
                 wprintf(L"Same packet received count: %d\n", ++same_packet_received_count);
                 Warning("already received packet number " + std::to_string(received_num));
                 continue;
@@ -327,21 +323,21 @@ int main()
             is_received[received_num] = true;
             // UPDATE ACK NUMBER AND PRINT
             while (ack_number + 1 < packets_count && is_received[ack_number + 1]) { // look one ahead if received
-                setPrintPos(indexToPos(ack_number)); // previous ack
+                SetPrintPos(IndexToPos(ack_number)); // previous ack
                 wprintf(L"▓");
                 ack_number++; // update ack
             }
-            setPrintPos(indexToPos(ack_number)); // current ack
+            SetPrintPos(IndexToPos(ack_number)); // current ack
             wprintf(L"█");
             // SEND ACK
             SendACK(ack_number);
             // IF RECEIVED PACKET IS STILL AHEAD OF ACK PRINT SOFT SQUARE
             if (received_num > ack_number) {
-                setPrintPos(indexToPos(received_num));
+                SetPrintPos(IndexToPos(received_num));
                 wprintf(L"▒");
             }
             // PRINT ACK NUMBER
-            setPrintPos(0, Y_NUMBER); // current ack
+            SetPrintPos(0, Y_NUMBER); // current ack
             wprintf(L"| ACK %d / %d |", ack_number, packets_count);
             // BREAK IF END
             if (ack_number == packets_count - 1) {
@@ -380,7 +376,7 @@ int main()
             // CLEAR SCREEN
             std::wstring spaces = std::wstring(screen_width, ' ');
             for (int i = 0; i < 100; ++i) {
-                setPrintPos(0, i);
+                SetPrintPos(0, i);
                 wprintf(L"%s", spaces.c_str());
             }
         }
@@ -400,15 +396,15 @@ int main()
 //for (int i = 0; i < packets_count; ++i) {
 //    // NASTAV ACK NA NEJAKOU HODNOTU
 //    ack_number++;
-//    Info("cursor = " + std::to_string(cursor) + " x = " + std::to_string(indexToPos(cursor).X) + " y = " + std::to_string(indexToPos(cursor).Y));
+//    Info("cursor = " + std::to_string(cursor) + " x = " + std::to_string(IndexToPos(cursor).X) + " y = " + std::to_string(IndexToPos(cursor).Y));
 //    // FUNKCE NA DOPLNENI semi transp a posledniho transp k poslednimu ack number
 //    while (cursor < ack_number) {
-//        setPrintPos(indexToPos(cursor));
+//        SetPrintPos(IndexToPos(cursor));
 //        wprintf(L"▒▓");
 //        cursor++;
-//        Info("cursor = " + std::to_string(cursor) + " x = " + std::to_string(indexToPos(cursor).X) + " y = " + std::to_string(indexToPos(cursor).Y));
+//        Info("cursor = " + std::to_string(cursor) + " x = " + std::to_string(IndexToPos(cursor).X) + " y = " + std::to_string(IndexToPos(cursor).Y));
 //    }
-//    setPrintPos(indexToPos(cursor));
+//    SetPrintPos(IndexToPos(cursor));
 //    wprintf(L"█");
 //    Sleep(50);
 //}
